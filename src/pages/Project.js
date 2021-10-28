b22a2c71b803242e98930f1a689e4052db1924a7
Tyler Rice <tylerrice@Tylers-MBP-2.cable.rcn.com>
2021-10-28 17:08:52 -0500

    songs showing on projects page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,6 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 const Project = (props) => {
+
+    // TODO: heroku url
+
+    const [songs, setSongs] = useState([])
+
+    const API_URL_SONGS = 'http://localhost:3001/api/songs'
+
+    const getSongs = async () => {
+        const response = await fetch(API_URL_SONGS);
+        const songs = await response.json();
+        setSongs(songs);
+    };
+
+    useEffect(() => {
+        getSongs();
+    }, [])
+
+
     const id = props.match.params.id;
     const projects = props.projects;
     const project = projects.find(p => p._id === id);
@@ -10,6 +29,9 @@ const Project = (props) => {
         <main>
             <h1>Project</h1>
             <h2>{project.title}</h2>
+            {songs.map((song, idx) => (
+                <h1>{song.song.title}</h1>
+            ))}
         </main>
     );
 };
