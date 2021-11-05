50a8f85ab8c7bfa267bbbaab830a5e13b8427e74
Tyler Rice <tylerrice@Tylers-MBP-2.cable.rcn.com>
2021-11-04 22:10:33 -0500

    rid of errors

--- src/pages/Song.js
+++ src/pages/Song.js
@@ -3,11 +3,7 @@ import Header from "../components/Header";
 import { StyledSong } from "../styles";
 import { Button } from "@mui/material";
 import { TextField } from "@mui/material";
-import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 
 
